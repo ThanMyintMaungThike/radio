d83bbdf02441c89c4b1162f81300b8b4810a5901
ThanMyintMaungThike <thanmyintmaungthike@gmail.com>
2024-01-15 15:07:05 +0630

    top-banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import '../scss/style.scss';
+
+let btn = document.querySelector('.btn');
+let l_header = document.querySelector('.l-header__menu');
+
+btn.addEventListener('click', function(e) {
+        
+        l_header.classList.toggle('show');
+});
