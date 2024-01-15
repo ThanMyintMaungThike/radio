5a2f9fcef089150e480c7a45c5c136d62507dd16
pyaesone21 <pyaesone.puppey@gmail.com>
2024-01-15 14:32:46 +0630

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 import '../scss/style.scss';
+import jQuery from 'jquery';
+import 'slick-carousel'
+const $ = jQuery;
+
+$('.card-bloc--carousel').slick({
+  fade: false,
+  autoplay: false,
+  pauseOnHover: true,
+  autoplaySpeed: 5400,
+  speed: 300,
+  touchThreshold: 8,
+  variableWidth: false,
+  slidesToShow: 5,
+  arrows: true,
+  centerMode: true,
+  responsive: [{
+
+    breakpoint: 1320,
+    settings: {
+      slidesToShow: 3,
+    }
+  },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        infinite: true,
+      },
+    },
+  ],
+}); 
+
+
+
